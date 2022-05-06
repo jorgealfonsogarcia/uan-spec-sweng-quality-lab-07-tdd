a749703ce7882a125481aafc748cabc82185fd95
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-05-05 20:31:43 -0500

    GREEN-SUM-NuevaClaseMatematicaMetodoSumar

---
+++ src/main/java/co/edu/uan/quality/tdd/Matematica.java
@@ -0,0 +1,25 @@
+package co.edu.uan.quality.tdd;
+
+/**
+ * Operaciones matemáticas.
+ *
+ * @author Jorge Alfonso García Espinosa
+ * @author Diego Alejandro Poveda Sánchez
+ * @author Luis Carlos Ruiz Cárdenas
+ * @author Joan Sebastián Siabato Fonseca
+ * @version 1.0
+ * @since 11
+ */
+public class Matematica {
+
+    /**
+     * Suma dos números enteros.
+     *
+     * @param a Primer número entero.
+     * @param b Segundo número entero.
+     * @return La suma de los números enteros.
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
